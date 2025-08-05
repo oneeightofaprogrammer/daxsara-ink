3bf8b230a2f13893f0c4f2951eb44c24c0544c98
oneeightofaprogrammer <hesamoddinaran@gmail.com>
2025-08-05 07:06:57 +0330

    changed the .github/workflows/ to originals by docusaurus.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'daxsara', // Usually your GitHub org/user name.
+  organizationName: 'oneeightofaprogrammer', // Usually your GitHub org/user name.
   projectName: 'daxsara-ink', // Usually your repo name.
 
   onBrokenLinks: 'throw',
